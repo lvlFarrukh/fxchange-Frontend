2f45b8d59e8f37ca364acd87f2a5f2b9e388e236
FARRUKH ASLAM <farrukha303@gmail.com>
2021-10-09 20:20:03 +0500

    add moderator page

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,23 +1,22 @@
-
 import React, {Component} from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import SplashScreen from '../ui/screens/SplashScreen'
-import LandingScreen from '../ui/screens/LandingScreen';
+import {createStackNavigator} from '@react-navigation/stack';
+import SplashScreen from '../ui/screens/SplashScreen/SplashScreen';
+import LandingScreen from '../ui/screens/LandingScreen/LandingScreen';
+import Moderator from '../ui/screens/Moderator/Home';
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
-    return (
-        <Stack.Navigator
-            initialRouteName="SplashScreen"
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
-            <Stack.Screen name="SplashScreen" component={SplashScreen} />
-            <Stack.Screen name="LandingScreen" component={LandingScreen} />
-
-        </Stack.Navigator>
-    )
-}
-export default Routes
+  return (
+    <Stack.Navigator
+      initialRouteName="SplashScreen"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      <Stack.Screen name="LandingScreen" component={LandingScreen} />
+      <Stack.Screen name="ModeratorScreen" component={Moderator} />
+    </Stack.Navigator>
+  );
+};
+export default Routes;
---
+++ src/ui/screens/Moderator/Home/index.tsx
@@ -0,0 +1,152 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable semi */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, {useContext, useEffect, useMemo, useState} from 'react';
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  TextInput,
+  Dimensions,
+  TouchableHighlight,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+const {width, height} = Dimensions.get('screen');
+const btnSetected: any = {
+  backgroundColor: '#0a8a40',
+  color: 'white',
+  padding: '2.5%',
+};
+
+const index = () => {
+  const [pendingBtn, setpendingBtn] = useState({});
+  const [ongoingBtn, setOngoing] = useState(btnSetected);
+  const [CompletedBtn, setCompleted] = useState({});
+
+  const activeButton = (buttonType: string) => {
+    console.log(buttonType);
+    if (buttonType === 'pending') {
+      setpendingBtn(btnSetected);
+      setOngoing({});
+      setCompleted({});
+    }
+    if (buttonType === 'ongoing') {
+      setpendingBtn({});
+      setOngoing(btnSetected);
+      setCompleted({});
+    }
+    if (buttonType === 'completed') {
+      setpendingBtn({});
+      setOngoing({});
+      setCompleted(btnSetected);
+    }
+  };
+  // eslint-disable-next-line prettier/prettier
+  return (
+    <SafeAreaView
+      style={{
+        height: height,
+        width: width,
+        flex: 1,
+      }}>
+      <View style={styles.header}>
+        <Text style={styles.headerHeading}>Trades</Text>
+      </View>
+
+      <View style={styles.mainBody}>
+        <View style={[styles.headerButtons]}>
+          <Text
+            style={[styles.headerButtom, styles.JoinLeft, {...pendingBtn}]}
+            onPress={() => {
+              activeButton('pending');
+            }}>
+            Pending
+          </Text>
+
+          <Text
+            style={[styles.headerButtom, {...ongoingBtn}]}
+            onClick={() => {
+              activeButton('ongoing');
+            }}>
+            Ongoing
+          </Text>
+
+          <Text
+            style={[styles.headerButtom, styles.JoinRight, {...CompletedBtn}]}
+            onPress={() => {
+              activeButton('completed');
+            }}>
+            Completed
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => {
+            console.log('Working');
+          }}>
+          <Text>Checking</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: width,
+    height: 100,
+    backgroundColor: '#0a8a40',
+  },
+  headerHeading: {
+    color: 'white',
+    textAlign: 'center',
+    marginTop: '3.5%',
+    fontSize: 16,
+  },
+  mainBody: {
+    alignSelf: 'center',
+    width: width,
+    height: height - 100,
+    backgroundColor: '#f2f2f2',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    position: 'absolute',
+    zIndex: 1,
+    top: '10%',
+  },
+  headerButtons: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: '6.5%',
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 30,
+    flexDirection: 'row',
+    marginTop: 30,
+    justifyContent: 'center',
+  },
+  headerButtom: {
+    fontSize: 13,
+    textAlign: 'center',
+    width: '42%',
+    borderRadius: 30,
+    color: '#0a8a40',
+    padding: '3%',
+  },
+  JoinLeft: {
+    position: 'relative',
+    left: 25,
+  },
+  JoinRight: {
+    position: 'relative',
+    right: 25,
+  },
+});
+
+export default index;
